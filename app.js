0cf08d556f48474bfd6c8c9ea95633b69411b011
nac401 <nac401@nyu.edu>
2025-08-22 21:20:14 -0400

    work page start

--- app.js
+++ app.js
@@ -5,3 +5,24 @@ menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 })
+
+const filterButtons = document.querySelectorAll(".filter__buttons button");
+const filterableCards = document.querySelectorAll(".filterable__cards .card");
+
+const filterCards = e => {
+    document.querySelector(".active").classList.remove("active");
+    e.target.classList.add("active")
+
+    filterableCards.forEach(card => {
+        card.classList.add("hide");
+        var dataList = card.dataset.name.split(',')
+        for (i = 0; i < dataList.length; i++) {
+            if(dataList[i] === e.target.dataset.name ||  e.target.dataset.name === "all") {
+                card.classList.remove("hide")
+            }
+        }
+    })
+    
+}
+
+filterButtons.forEach(button => button.addEventListener("click", filterCards))
