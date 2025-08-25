e00c105ca1a67f0b8e205cba7a95784e96b02880
nac401 <nac401@nyu.edu>
2025-08-25 17:58:47 -0400

    skeleton layout

--- js/nav.js
+++ js/nav.js
@@ -1,8 +1,17 @@
-fetch('nav.html')
+fetch('persistents/nav.html')
 .then(res => res.text())
 .then(text => {
     let oldelem = document.querySelector("script#replace_with_navbar");
     let newelem = document.createElement("div");
     newelem.innerHTML = text;
+
+    const menu = newelem.querySelector('#mobile-menu')
+    const menuLinks = newelem.querySelector('.navbar__menu')
+    
+    menu.addEventListener('click', function() {
+        menu.classList.toggle('is-active');
+        menuLinks.classList.toggle('active');
+    })
+
     oldelem.parentNode.replaceChild(newelem,oldelem);
 })
--- js/work.js
+++ js/work.js
@@ -1,11 +1,3 @@
-const menu = document.querySelector('#mobile-menu')
-const menuLinks = document.querySelector('.navbar__menu')
-
-menu.addEventListener('click', function() {
-    menu.classList.toggle('is-active');
-    menuLinks.classList.toggle('active');
-})
-
 const filterButtons = document.querySelectorAll(".filter__buttons button");
 const filterableCards = document.querySelectorAll(".filterable__cards .card");
 
